a759c955e34fed52830b334b0d5c1bb1362d5728
Danilo Croce <croce@info.uniroma2.it>
2016-06-13 15:57:42 +0200

    Removed unused import

--- src/test/java/it/uniroma2/sag/kelp/kernel/tree/TreeKernelTest.java
+++ src/test/java/it/uniroma2/sag/kelp/kernel/tree/TreeKernelTest.java
@@ -20,7 +20,6 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.zip.GZIPInputStream;
